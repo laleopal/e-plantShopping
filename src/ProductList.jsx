107847499bb684e492ab59ad8ce14d2d3b243a3b
laleopal <opal.lale@gmail.com>
2024-09-20 10:43:37 -0400

    disable add to cart button

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -292,7 +292,10 @@ const handlePlantsClick = (e) => {
                                 <div className="product-title">{plant.name}</div>
                                 <div className="product-description">{plant.description}</div>
                                 <div className="product-price">{plant.cost}</div>
-                                <button className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                                <button 
+                                    className={addedToCart[plant.name] ? "product-button.added-to-cart" : "product-button"} 
+                                    onClick={() => handleAddToCart(plant)} 
+                                    disabled={addedToCart[plant.name]}>Add to Cart</button>
                             </div>
                         ))}
                     </div>
